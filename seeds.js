27205f1f1b37ad2f3c381213bb8ce18a75df0d44
Your Name <azmi.bengkalis@gmail.com>
2024-09-10 17:02:53 +0700

    Commit pertama kali

---
+++ seeds.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const Product = require("./models/product");
+
+mongoose
+  .connect("mongodb://127.0.0.1/shop_db")
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const seedProducts = [
+  {
+    name: "Kemeja Flanel",
+    brand: "Hollister",
+    price: 750000,
+    color: "biru muda",
+    size: "M",
+  },
+
+  {
+    name: "Sweater",
+    brand: "Gap",
+    price: 650000,
+    color: "merah muda",
+    size: "M",
+  },
+
+  {
+    name: "Baju Renang",
+    brand: "Speedo",
+    price: 500000,
+    color: "biru tua",
+    size: "XL",
+  },
+  {
+    name: "Topi Baseball",
+    brand: "New Era",
+    price: 350000,
+    color: "hitam",
+    size: "L",
+  },
+  {
+    name: "Rompi",
+    brand: "Zara",
+    price: 850000,
+    color: "abu-abu",
+    size: "S",
+  },
+  {
+    name: "Jas",
+    brand: "Hugo Boss",
+    price: 4500000,
+    color: "hitam",
+    size: "L",
+  },
+];
+
+Product.insertMany(seedProducts)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
